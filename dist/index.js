9df71b191c08d43b3c0b1b667fe1b7bf6a9a1689
Mateusz Cholewka <mateusz.cholewka@owlsdepartment.com>
2019-03-13 16:02:15 +0100

    add data key

--- dist/index.js
+++ dist/index.js
@@ -68,7 +68,7 @@ class Token {
     return new Promise((resolve, reject) => {
       axiosInstance.post(this.refreshUrl, { token: this.getToken() })
         .then(response => {
-          const token = response.data.access_token;
+          const token = response.data.data.access_token;
           this.setToken(token);
           resolve(token);
         })
